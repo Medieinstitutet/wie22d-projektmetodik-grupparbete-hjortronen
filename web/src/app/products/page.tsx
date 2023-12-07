a100184a161b20143cb1dfee8977e8bae47a2c50
Martin Johansson <mjohansson176@gmail.com>
2023-12-07 15:21:51 +0100

    Produktkort placeholder klart

--- web/src/app/products/page.tsx
+++ web/src/app/products/page.tsx
@@ -1,5 +1,38 @@
 import React from "react";
 
-export default function products() {
-  return <div>productspage</div>;
+import ProductCard from "../../components/ProductCard/productCard";
+
+interface IProduct {
+  id: string;
+  title: string;
+  description: string;
+  category: string;
+  img: string;
+  imgAlt: string;
+  price: number;
+}
+
+export default async function products() {
+
+  const res = await fetch("http://localhost:3000/products", {cache: 'no-store'})
+  const products = await res.json()  
+  console.log(products);
+  
+
+  return <>
+    <p>Productspage</p>
+
+    <div className="flex flex-wrap w-3/4">
+      {products.map((product: IProduct) => (
+        <div className="ProductCardRender" key={product.id}>
+          
+            <ProductCard product={product} />
+          
+        </div>
+      ))}
+    </div>    
+
+  
+  </>
+  
 }
