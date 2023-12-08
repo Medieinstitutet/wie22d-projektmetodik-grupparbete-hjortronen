e0b21ee66b71c69321d9e0363505c422df0d9427
Martin Johansson <mjohansson176@gmail.com>
2023-12-08 10:51:59 +0100

    Closes #13 Singelsida klar, startsida uppdaterad

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,27 @@
-export default function Home() {
+import Link from "next/link";
+import ProductCard from "../components/ProductCard/productCard";
+import IProduct from "@/interface/IProductCard";
+
+export default async function Home() {
+
+
+
+  const res = await fetch("http://localhost:3000/products", {cache: 'no-store'})
+  const products = await res.json()  
 
   return (
     <>
       <h1>Startpage</h1>
+      <div className="flex flex-wrap w-3/4">
+      {products.map((product: IProduct) => (
+        <div className="ProductCardRender" key={product.id}>
+          <Link href={`/products/${product.id}`}>
+            <ProductCard product={product} />
+          </Link>
+          
+        </div>
+      ))}
+    </div> 
     </>
   );
 
