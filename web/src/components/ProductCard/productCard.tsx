be7cd7f1201c1c0ead0846c30737e31095d759a2
Your name hereOscarwarmark <oscar.warmark@gmail.com>
2023-12-14 14:52:32 +0100

    mer padding fix

--- web/src/components/ProductCard/productCard.tsx
+++ web/src/components/ProductCard/productCard.tsx
@@ -6,7 +6,7 @@ import Button from "../button/button";
 
 function ProductCard({ product }: { product: IProduct }) {
   return (
-    <div className=" w-80 m-10 h-96 rounded-xl shadow-[3px_6px_11px_6px_#00000024]">
+    <div className="flex flex-col justify-between w-80 m-10 h-96 pb-5 rounded-xl shadow-[3px_6px_11px_6px_#00000024]">
       <Image
         src={product.img}
         width={400}
@@ -14,12 +14,12 @@ function ProductCard({ product }: { product: IProduct }) {
         alt={product.imgAlt}
         className="pl-14 pt-4 pr-14 "
       />
-      <div className="flex-col pr-9 pl-9 pt-2">
+      <div className="flex-col pr-7 pl-7 pt-2">
         <div className="pb-6">
           <h1 className="text-2xl">{product.title}</h1>
         </div>
-        <div className="flex justify-between bottom-0">
-          <p className="pl-1 text-xl">{product.price}:-</p>
+        <div className="flex justify-between ">
+          <p className="pl-1 pt-1 text-xl ">{product.price}:-</p>
           <Button>
             <AddToCartButton product={product} />
           </Button>
